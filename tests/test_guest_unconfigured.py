f0807a90ace90d60d130d0b7747bd9cd3e26ec84
Andrey Yantsen <andrey-yantsen@users.noreply.github.com>
2023-05-14 23:29:27 +0100

    test: test that /tours responds for unconfigured bot
    It's not really intentional, but already an existing behaviour.

--- tests/test_guest_unconfigured.py
+++ tests/test_guest_unconfigured.py
@@ -9,3 +9,13 @@ async def test_start_to_unconfigured_bot(
     assert (
         "The bot is not configured yet" in response.message
     ), "Unexpected response from an unconfigured bot"
+
+
+async def test_tours_to_unconfigured_bot(
+    conversation: Conversation, unconfigured_app, bot_token
+):
+    await conversation.send_message("/tours")
+    response = await conversation.get_response()
+    assert (
+        "no tours are available for you" in response.message
+    ), "Unexpected response from an unconfigured bot"
