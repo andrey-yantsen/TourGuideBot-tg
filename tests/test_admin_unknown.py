b22051ac091021716c4c821405f6e4b3bc63fbfa
Andrey Yantsen <andrey-yantsen@users.noreply.github.com>
2023-05-16 23:02:19 +0100

    test: cover sending a random contact during authentication

--- tests/test_admin_unknown.py
+++ tests/test_admin_unknown.py
@@ -1,6 +1,7 @@
 import pytest
 from telethon.tl.custom import Message
 from telethon.tl.custom.conversation import Conversation
+from telethon.tl.types import InputMediaContact
 
 from .conftest import get_phone_number_request
 
@@ -45,3 +46,18 @@ async def test_success_auth_flow_unconfigured_app(
     assert (
         "Admin permissions confirmed" in response.message
     ), "Unexpected message in response to the secret phrase"
+
+
+@pytest.mark.usefixtures("app")
+async def test_accepts_only_current_contact(conversation: Conversation):
+    await conversation.send_message("/admin")
+    response: Message = await get_phone_number_request(conversation)
+
+    await response.click(
+        0, share_phone=InputMediaContact("+79999999999", "Random", "Name", "")
+    )
+
+    response: Message = await conversation.get_response()
+    assert (
+        "Please send me your contact number and not somebody else's" in response.message
+    ), "Unexpected response to sanding a random contact"
--- tests/test_guest_unknown.py
+++ tests/test_guest_unknown.py
@@ -1,6 +1,7 @@
 import pytest
 from telethon.tl.custom import Message
 from telethon.tl.custom.conversation import Conversation
+from telethon.tl.types import InputMediaContact
 
 from .conftest import get_phone_number_request
 
@@ -21,3 +22,22 @@ async def test_success_auth_flow(conversation: Conversation):
         "Unfortunately, no tours are available for you at the moment"
         in response.message
     ), "Unexpected response to a user without tours"
+
+
+@pytest.mark.usefixtures("app")
+async def test_accepts_only_current_contact(conversation: Conversation):
+    await conversation.send_message("/start")
+    response: Message = await conversation.get_response()
+    assert (
+        response.message == "welcome"
+    ), "Unexpected first message from a configured bot"
+
+    response: Message = await get_phone_number_request(conversation)
+    await response.click(
+        0, share_phone=InputMediaContact("+79999999999", "Random", "Name", "")
+    )
+
+    response: Message = await conversation.get_response()
+    assert (
+        "Please send me your contact number and not somebody else's" in response.message
+    ), "Unexpected response to sanding a random contact"
