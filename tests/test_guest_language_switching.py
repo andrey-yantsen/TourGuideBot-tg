400c1f25ed423bb8038ce1bbbd98fd2407112623
Andrey Yantsen <andrey-yantsen@users.noreply.github.com>
2023-05-16 23:31:09 +0100

    test: add wait for MessageEdited on language switching

--- tests/test_guest_language_switching.py
+++ tests/test_guest_language_switching.py
@@ -1,4 +1,5 @@
 import pytest
+from telethon.events import MessageEdited
 from telethon.tl.custom import Message
 from telethon.tl.custom.conversation import Conversation
 from telethon.tl.types.messages import BotCallbackAnswer
@@ -27,6 +28,7 @@ async def test_multiple_languages(conversation: Conversation):
     assert isinstance(
         response, BotCallbackAnswer
     ), "BotCallbackAnswer didn't arrive after the inline button click"
+    await conversation.wait_event(MessageEdited())
 
     await conversation.send_message("/language")
     response: Message = await conversation.get_response()
