3eec36901f7105f40e44a3aafee1e7f73fa86a9a
Andrey Yantsen <andrey-yantsen@users.noreply.github.com>
2023-05-18 00:15:00 +0100

    test: cover incorrect messages during auth

--- tests/test_admin_unknown.py
+++ tests/test_admin_unknown.py
@@ -62,3 +62,24 @@ async def test_accepts_only_current_contact(conversation: Conversation):
     assert (
         "Please send me your contact number and not somebody else's" in response.message
     ), "Unexpected response to sending a random contact"
+
+
+@pytest.mark.usefixtures("app")
+async def test_incorrect_token(conversation: Conversation):
+    await conversation.send_message("/admin")
+    response: Message = await get_phone_number_request(conversation)
+    await response.click(0, share_phone=True)
+    await sleep(1)
+
+    response: Message = await conversation.get_response()
+    assert (
+        "Please send me the token to confirm ownership" in response.message
+    ), "Unexpected response to an unknown phone number"
+
+    await conversation.send_message("foo")
+
+    response: Message = await conversation.get_response()
+
+    assert (
+        "I still don't recognise you, sorry." in response.message
+    ), "Unexpected message in response to the secret phrase"
--- tests/test_guest_unknown.py
+++ tests/test_guest_unknown.py
@@ -1,5 +1,3 @@
-from asyncio import sleep
-
 import pytest
 from telethon.tl.custom import Message
 from telethon.tl.custom.conversation import Conversation
@@ -17,7 +15,6 @@ async def test_success_auth_flow(conversation: Conversation):
 
     response: Message = await get_phone_number_request(conversation)
     await response.click(0, share_phone=True)
-    await sleep(0.2)
 
     response: Message = await conversation.get_response()
     assert (
@@ -36,9 +33,31 @@ async def test_accepts_only_current_contact(conversation: Conversation):
 
     response: Message = await get_phone_number_request(conversation)
     await response.click(0, share_phone="+99999999999")
-    await sleep(0.2)
 
     response: Message = await conversation.get_response()
     assert (
         "Please send me your contact number and not somebody else's" in response.message
     ), "Unexpected response to sending a random contact"
+
+
+@pytest.mark.usefixtures("app")
+async def test_incorrect_commands_during_auth(conversation: Conversation):
+    await conversation.send_message("/start")
+    response: Message = await conversation.get_response()
+    assert (
+        response.message == "welcome"
+    ), "Unexpected first message from a configured bot"
+
+    await get_phone_number_request(conversation)
+
+    await conversation.send_message("test")
+    response: Message = await conversation.get_response()
+    assert (
+        "Please send me your phone" in response.message
+    ), "Unexpected response to an unexpected message during auth"
+
+    await conversation.send_message("/test")
+    response: Message = await conversation.get_response()
+    assert (
+        "Unexpected command received" in response.message
+    ), "Unexpected response to an unexpected command during auth"
