349df53532573c4295a9011f35a2596b5c3caebf
Andrey Yantsen <andrey-yantsen@users.noreply.github.com>
2023-05-17 20:49:38 +0100

    test: add basic test for tour`s beginning

---
+++ tests/test_guest_with_tours_basics.py
@@ -0,0 +1,223 @@
+import pytest
+from telethon.tl.custom import Message
+from telethon.tl.custom.conversation import Conversation
+from telethon.tl.types import MessageMediaGeo
+from telethon.tl.types.messages import BotCallbackAnswer
+
+from tour_guide_bot.models.guide import MessageType
+
+
+@pytest.mark.approved_tour_ids(1)
+@pytest.mark.usefixtures("app", "guest", "approved_tours")
+async def test_single_tour(conversation: Conversation, default_tour: dict):
+    await conversation.send_message("/start")
+    response: Message = await conversation.get_response()
+    assert (
+        response.message == "welcome"
+    ), "Unexpected first message from a configured bot"
+
+    response: Message = await conversation.get_response()
+    assert (
+        "I see you have some tours available" in response.message
+    ), "Unexpected response to a user with a single tour"
+
+    await conversation.send_message("/tours")
+    response: Message = await conversation.get_response()
+    assert (
+        "One tour available for you: %s" % (default_tour["en"]["title"])
+    ) in response.message, "Unexpected response to /tours"
+
+    for content in default_tour["en"]["sections"][0]["content"]:
+        response: Message = await conversation.get_response()
+
+        match content["type"]:
+            case MessageType.text:
+                assert (
+                    response.message == content["content"]["text"]
+                ), "Unexpected response message text"
+
+            case MessageType.location:
+                assert response.media is not None
+                assert isinstance(response.media, MessageMediaGeo)
+                assert round(response.media.geo.lat, 3) == round(
+                    content["content"]["latitude"], 3
+                )
+                assert round(response.media.geo.long, 3) == round(
+                    content["content"]["longitude"], 3
+                )
+
+            case _:
+                assert False, "Unsupported message type"
+
+    response: Message = await conversation.get_response()
+
+    assert (
+        "Are you ready to continue?" == response.message
+    ), "Unexpected message after the end of a section"
+
+
+@pytest.mark.approved_tour_ids(1, 2)
+@pytest.mark.tours(
+    {
+        "en": {
+            "title": "Test tour 1",
+            "sections": [
+                {
+                    "title": "Test section 1.1",
+                    "content": [
+                        {
+                            "type": MessageType.text,
+                            "content": {"text": r"Test text 1\.1\.1"},
+                        },
+                    ],
+                },
+                {
+                    "title": "Test section 1.2",
+                    "content": [
+                        {
+                            "type": MessageType.text,
+                            "content": {"text": r"Test text 1\.2\.1"},
+                        },
+                    ],
+                },
+            ],
+        }
+    },
+    {
+        "en": {
+            "title": "Test tour 2",
+            "sections": [
+                {
+                    "title": "Test section 2.1",
+                    "content": [
+                        {
+                            "type": MessageType.text,
+                            "content": {"text": r"Test text 2\.1\.1"},
+                        },
+                    ],
+                },
+                {
+                    "title": "Test section 2.2",
+                    "content": [
+                        {
+                            "type": MessageType.text,
+                            "content": {"text": r"Test text 2\.2\.1"},
+                        },
+                    ],
+                },
+            ],
+        }
+    },
+)
+@pytest.mark.usefixtures("app", "guest", "approved_tours")
+async def test_multiple_tours(conversation: Conversation):
+    await conversation.send_message("/start")
+    response: Message = await conversation.get_response()
+    assert (
+        response.message == "welcome"
+    ), "Unexpected first message from a configured bot"
+
+    response: Message = await conversation.get_response()
+    assert (
+        "I see you have some tours available" in response.message
+    ), "Unexpected response to a user with a single tour"
+
+    await conversation.send_message("/tours")
+    response: Message = await conversation.get_response()
+    assert "Please select a tour you want to start." == response.message
+
+    response = await response.click(text="Test tour 2")
+    assert isinstance(response, BotCallbackAnswer)
+
+    response: Message = await conversation.get_response()
+    assert (
+        "Test text 2.1.1" == response.message
+    ), "Unexpected first section of the tour 2"
+
+    response: Message = await conversation.get_response()
+    assert (
+        "Are you ready to continue?" == response.message
+    ), "Unexpected message after the end of a section"
+
+
+@pytest.mark.approved_tour_ids(2)
+@pytest.mark.tours(
+    {
+        "en": {
+            "title": "Test tour 1",
+            "sections": [
+                {
+                    "title": "Test section 1.1",
+                    "content": [
+                        {
+                            "type": MessageType.text,
+                            "content": {"text": r"Test text 1\.1\.1"},
+                        },
+                    ],
+                },
+                {
+                    "title": "Test section 1.2",
+                    "content": [
+                        {
+                            "type": MessageType.text,
+                            "content": {"text": r"Test text 1\.2\.1"},
+                        },
+                    ],
+                },
+            ],
+        }
+    },
+    {
+        "en": {
+            "title": "Test tour 2",
+            "sections": [
+                {
+                    "title": "Test section 2.1",
+                    "content": [
+                        {
+                            "type": MessageType.text,
+                            "content": {"text": r"Test text 2\.1\.1"},
+                        },
+                    ],
+                },
+                {
+                    "title": "Test section 2.2",
+                    "content": [
+                        {
+                            "type": MessageType.text,
+                            "content": {"text": r"Test text 2\.2\.1"},
+                        },
+                    ],
+                },
+            ],
+        }
+    },
+)
+@pytest.mark.usefixtures("app", "guest", "approved_tours")
+async def test_multiple_tours_one_approved(conversation: Conversation):
+    await conversation.send_message("/start")
+    response: Message = await conversation.get_response()
+    assert (
+        response.message == "welcome"
+    ), "Unexpected first message from a configured bot"
+
+    response: Message = await conversation.get_response()
+    assert (
+        "I see you have some tours available" in response.message
+    ), "Unexpected response to a user with a single tour"
+
+    await conversation.send_message("/tours")
+    response: Message = await conversation.get_response()
+    assert (
+        "One tour available for you: Test tour 2"
+    ) in response.message, "Unexpected response to /tours"
+
+    response: Message = await conversation.get_response()
+    assert (
+        "Test text 2.1.1" == response.message
+    ), "Unexpected first section of the tour 2"
+
+    response: Message = await conversation.get_response()
+    assert (
+        "Are you ready to continue?" == response.message
+    ), "Unexpected message after the end of a section"
